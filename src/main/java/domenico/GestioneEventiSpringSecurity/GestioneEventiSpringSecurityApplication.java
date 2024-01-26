40c49b92913177b15ae6d97d83a69e0bbd25e467
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-01-26 10:48:07 +0100

    struttura spring

---
+++ src/main/java/domenico/GestioneEventiSpringSecurity/GestioneEventiSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package domenico.GestioneEventiSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/domenico/GestioneEventiSpringSecurity/GestioneEventiSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package domenico.GestioneEventiSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
